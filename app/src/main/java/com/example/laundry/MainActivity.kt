ce50b9a997ec1f089daa94cad2be4923a2517b83
Yzkrobn <oktavianbagasnugroho@gmailcom>
2025-01-24 08:35:25 +0700

    Segment 3 : penambahan activity data pelanggan

--- app/src/main/java/com/example/laundry/MainActivity.kt
+++ app/src/main/java/com/example/laundry/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.laundry
 
+import android.annotation.SuppressLint
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    lateinit var welcome_text_TV:TextView
+    lateinit var waktu_text_TV: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        init()
+        pewaktu()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun init(){
+        welcome_text_TV = findViewById(R.id.welcome_text_TV)
+        waktu_text_TV = findViewById(R.id.waktu_text_TV)
+    }
+    @SuppressLint("SimpleDateFormat")
+    private fun pewaktu() {
+        val jam = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val hth = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        val result = when (jam) {
+            in 0..11 -> "Selamat Pagi"
+            in 12..15 -> "Selamat Siang"
+            in 16..18 -> "Selamat Sore"
+            else -> "Selamat Malam"
+        }
+        welcome_text_TV.text = result
+        waktu_text_TV.text = hth.format(Calendar.getInstance().time)
     }
 }
